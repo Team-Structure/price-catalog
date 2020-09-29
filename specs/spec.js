b1c1a03ff2f7744374bdb76b5577ff24b6efb374
Amit Bhogle <abhogle17@gmail.com>
2020-09-29 00:50:46 -0600

    Initial DB setup complete
    Seeding script setup
    Unit test added to test both prices, and sellers documents are seeding

--- specs/spec.js
+++ specs/spec.js
@@ -1,8 +1,35 @@
-const expect = require('chai').expect;
+/* eslint-disable no-undef */
+const { expect } = require('chai');
+const mongoose = require('mongoose');
+const { Price } = require('../database/models/prices');
+const { Seller } = require('../database/models/sellers');
 
-describe('Sample Test', () => {
-  it('Sample Test 1', () => {
-    const result = true;
-    expect(result).to.equal(true);
+describe('Database seeded', () => {
+  before(() => {
+    mongoose.connect('mongodb://localhost/seller-catalog', { useMongoClient: true });
+  });
+  after(() => {
+    mongoose.disconnect();
+  });
+
+  it('Database seeded with 100 Products', () => {
+    let productCount = 0;
+    Price.find()
+      .then((data) => {
+        mongoose.disconnect();
+        productCount = data.length;
+        expect(productCount).to.equal(100);
+      });
+  });
+
+  it('Database seeded with 10 Sellers', () => {
+    // mongoose.connect('mongodb://localhost/seller-catalog', { useMongoClient: true });
+    // const db = mongoose.connection;
+    let sellerCount = 0;
+    Seller.find()
+      .then((data) => {
+        sellerCount = data.length;
+        expect(sellerCount).to.equal(10);
+      });
   });
 });
