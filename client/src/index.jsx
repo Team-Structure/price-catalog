b22928cab1b7de0facb791a7b9986d1aae5bc033
Amit Bhogle <abhogle17@gmail.com>
2020-10-10 15:37:10 -0600

    Added Quick checkout button

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -21,33 +21,53 @@ const App = () => (
         </div>
       </div>
       <div className="buttonSpace">
-        <div className="visitButton">Visit site</div>
+        <div className="actionButton actionColor actionColor">
+          Visit site
+        </div>
       </div>
     </div>
     <div className="sellerOption">
       <div>
-        <div className="total">$15.15</div>
-        <div>+$1.23 est. tax</div>
-        <div>Free delivery</div>
+        <div className="total">$16.99</div>
+        <div>+$1.61 est. tax</div>
+        <div>Spend $20 for free delivery by Mon, Oct 19</div>
+        <div>Return-eligible for 30 days</div>
         <div className="store">
-          Walmart
+          Stuff From India
         </div>
       </div>
       <div className="buttonSpace">
-        <div className="visitButton">Visit site</div>
+        <div className="fRight">
+          <div className="actionButton actionColor">
+            <span className="cartSymbol" />
+            Add to cart
+          </div>
+          <div className="actionButton checkoutColor">
+            Quick checkout
+          </div>
+        </div>
       </div>
     </div>
     <div className="sellerOption">
       <div>
-        <div className="total">$15.15</div>
-        <div>+$1.23 est. tax</div>
-        <div>Free delivery</div>
+        <div className="total">$27.63</div>
+        <div>+$2.23 est. tax</div>
+        <div>Free delivery by Wed, Oct 21</div>
+        <div>Return-eligible for 30 days</div>
         <div className="store">
-          Walmart
+          Swiftsy
         </div>
       </div>
       <div className="buttonSpace">
-        <div className="visitButton">Visit site</div>
+        <div className="fRight">
+          <div className="actionButton actionColor">
+            <span className="cartSymbol" />
+            Add to cart
+          </div>
+          <div className="actionButton checkoutColor">
+            Quick checkout
+          </div>
+        </div>
       </div>
     </div>
   </div>
