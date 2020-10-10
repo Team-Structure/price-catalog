22b73c2f046701496dd60c3579003c20521b7b3d
Amit Bhogle <abhogle17@gmail.com>
2020-10-10 14:46:06 -0600

    Client WIP Created container Created scaffolding to populate pricing info WIP format and buttons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>Hello React</div>;
+const App = () => (
+  <div id="container">
+    <div>
+      <div id="element1">
+        Buying options
+        <div className="fRight">
+          <div id="about">About &#9432;</div>
+        </div>
+      </div>
+    </div>
+    <div className="sellerOption">
+      <div>
+        <div className="total">$15.15</div>
+        <div>+$1.23 est. tax</div>
+        <div>Free delivery</div>
+        <div className="store">
+          Walmart
+        </div>
+      </div>
+      <div className="buttonSpace">
+        <div className="visitButton">Visit site</div>
+      </div>
+    </div>
+    <div className="sellerOption">
+      <div>
+        <div className="total">$15.15</div>
+        <div>+$1.23 est. tax</div>
+        <div>Free delivery</div>
+        <div className="store">
+          Walmart
+        </div>
+      </div>
+      <div className="buttonSpace">
+        <div className="visitButton">Visit site</div>
+      </div>
+    </div>
+    <div className="sellerOption">
+      <div>
+        <div className="total">$15.15</div>
+        <div>+$1.23 est. tax</div>
+        <div>Free delivery</div>
+        <div className="store">
+          Walmart
+        </div>
+      </div>
+      <div className="buttonSpace">
+        <div className="visitButton">Visit site</div>
+      </div>
+    </div>
+  </div>
+);
 
 // class App extends React.Component {
 //   constructor(props) {
